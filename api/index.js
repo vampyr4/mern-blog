6f9d86a684c9cd70d1b13ee3d82e284c1fb69651
Abdul Wahab Khan <asad49033@gmail.com>
2024-09-15 16:09:35 +0500

    connected database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express()
 
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Database is up and running!")
+})
 app.listen(5000,()=>{
     console.log("Server is running on port:5000")
-})
+})  
