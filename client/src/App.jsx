bc5a352235f6b578b2dff1d0c1fbde7cc6919026
Abdul Wahab Khan <asad49033@gmail.com>
2024-09-16 18:27:34 +0500

    add private route component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp"
 import Home from "./pages/Home"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import PrivateRoute from "./components/PrivateRoute"
 
 export default function App() {
   return (
@@ -16,7 +17,9 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
+        <Route element={<PrivateRoute />}>
         <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
