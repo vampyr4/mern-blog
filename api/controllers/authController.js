292d3f5db40742078f9f02cc6bb76bd91880a5af
Abdul Wahab Khan <asad49033@gmail.com>
2024-09-16 12:42:19 +0500

    Sign in api completed

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,21 +1,57 @@
 import User from "../models/userModel.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
 
-export const SignUp = async(req,res , next)=>{
-    const {username,email,password} = req.body
-    if(!username || !password || !email){
-        return next(errorHandler(400,"All Fields Required!"))
+export const SignUp = async (req, res, next) => {
+    const { username, email, password } = req.body
+    if (!username || !password || !email) {
+        return next(errorHandler(400, "All Fields Required!"))
     }
-    
-    const hashedPassword  = bcryptjs.hashSync(password,10)
-    const newUser = new User({username,password:hashedPassword,email})
+
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({ username, password: hashedPassword, email })
 
     //save the user
     try {
         await newUser.save()
-        res.status(201).json({message:"User registered successfully"})
+        res.status(201).json({ message: "User registered successfully" })
     } catch (error) {
         next(error)
     }
-}   
+}
+
+export const SignIn = async (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        next(errorHandler(400,"All fields are required!"))
+    }
+
+    try{
+        const validUser = await User.findOne({email})
+        // console.log(validUser);
+        
+        if(!validUser){
+           return next(errorHandler(404,"User not found"))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        // console.log(validPassword);
+        
+        if(!validPassword){
+           return next(errorHandler(401,"Incorrect Password"))
+        }
+        const token = jwt.sign({
+            id: validUser._id
+        },process.env.JWT_SEC)
+
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true
+        }).json({
+            success:true,
+            validUser
+        })
+    }
+    catch(error){
+        next(error)
+    }
+}
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { SignUp } from "../controllers/authController.js"
+import { SignUp,SignIn } from "../controllers/authController.js"
 
 const router = express.Router()
 
 router.post('/signup', SignUp )
+router.post('/signin', SignIn )
+
 
 export default router
