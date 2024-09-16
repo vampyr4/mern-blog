332d023a4e6064bea85c35583cb3ed57db784bb7
Abdul Wahab Khan <asad49033@gmail.com>
2024-09-16 14:36:39 +0500

    updated the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,65 +1,90 @@
-import { Navbar, TextInput,Button } from "flowbite-react"
-import { AiOutlineSearch } from "react-icons/ai"
-import { FaMoon } from "react-icons/fa"
-import { Link, useLocation } from "react-router-dom"
-
+import { Navbar, Dropdown, TextInput, Button, Avatar } from "flowbite-react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 function Header() {
   const location = useLocation();
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className="border-b-2 ">
-        <Link to={"/"} className="whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"> 
-            <span className="text-white px-2 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg">MERNs</span>
-            Blog
-        </Link>
+      <Link
+        to={"/"}
+        className="whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="text-white px-2 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg">
+          MERNs
+        </span>
+        Blog
+      </Link>
 
-        <form>
-            <TextInput 
-              placeholder="Search..."
-              type="text"
-              rightIcon={AiOutlineSearch}
-            className="hidden lg:inline"
-             />
-        </form>
+      <form>
+        <TextInput
+          placeholder="Search..."
+          type="text"
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
 
-             <Button className="w-12 h-10 lg:hidden" pill color='gray'>
-                <AiOutlineSearch />
-             </Button>
+      <Button className="w-12 h-10 lg:hidden" pill color="gray">
+        <AiOutlineSearch />
+      </Button>
 
-             <div className="flex gap-2 md:order-2">
-              <Button color="gray" className="w-12 h-10 hidden sm:inline" pill>
-                <FaMoon/>
-              </Button>
+      <div className="flex gap-2 md:order-2">
+        <Button color="gray" className="w-12 h-10 hidden sm:inline" pill>
+          <FaMoon />
+        </Button>
 
-              <Link to='/signin'>
-              <Button outline gradientDuoTone='purpleToBlue'>
-                Sign In
-              </Button>
-                
-              </Link>
-              <Navbar.Toggle>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="User Image"
+                img={currentUser.profilePicture}
+                rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
 
-              </Navbar.Toggle>
-             </div>
-              <Navbar.Collapse>
-                <Navbar.Link active={location === '/'} as="div">
-                  <Link to='/'>
-                    Home
-                  </Link>
-                </Navbar.Link>
-                <Navbar.Link active={location === '/about'} as="div">
-                  <Link to='/about'>
-                    About
-                  </Link>
-                </Navbar.Link>
-                <Navbar.Link active={location === '/projects'} as="div" >
-                  <Link to='/projects' >
-                    Projects
-                  </Link>
-                </Navbar.Link>
-
-              </Navbar.Collapse>
+            <Dropdown.Divider />
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Sign Out</Dropdown.Item>
+            </Link>
+          </Dropdown>
+        ) : (
+          <Link to="/signin">
+            <Button outline gradientDuoTone="purpleToBlue">
+              Sign In
+            </Button>
+          </Link>
+        )}
+        <Navbar.Toggle></Navbar.Toggle>
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={location === "/"} as="div">
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={location === "/about"} as="div">
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={location === "/projects"} as="div">
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
 
-export default Header
+export default Header;
