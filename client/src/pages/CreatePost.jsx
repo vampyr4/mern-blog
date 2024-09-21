6560d22857c847323dd5f50753ec3a5afcf867a2
Abdul Wahab Khan <asad49033@gmail.com>
2024-09-21 21:38:26 +0500

    upload image functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,12 +1,44 @@
 import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import { useState } from "react";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-
+import app from "../firebase.js"
+import { getStorage, ref, uploadBytesResumable,getDownloadURL } from "firebase/storage" 
 
 export default function CreatePost() {
+  const [image, setImage] = useState(null)
+  const [filename, setFilename] = useState("")
+  const [formData, setFormData] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  const HandleImage = (e) => {
+    setImage(e.target.files[0])
+    setFilename(e.target.files[0].name)
+  }
+
+  const HandleUpload = async(e) => {
+    e.preventDefault()
+    setLoading(true)
+    const storage = getStorage(app)
+    setFilename('image' + Math.random(3) + filename)
+    const storageRef = ref(storage, filename)
+    const uploadTask = await uploadBytesResumable(storageRef, image) 
+    console.log(uploadTask);
+    
+    if(!uploadTask.error){
+      const downloadURL = await getDownloadURL(uploadTask.ref)
+      console.log(downloadURL);
+      setFormData({...formData, image: downloadURL})
+      setFilename(null)
+      setImage(null)
+      setLoading(false)
+    }
+
+    setLoading(false)
+  }
   return (
     <div className="p-3 flex flex-col max-w-3xl mx-auto min-h-screen">
-      <h1 className="text-center text-3xl my-7 font-semibold">Create a post!</h1>
+      <h1 className="text-ce nter text-3xl my-7 font-semibold">Create a post!</h1>
       <form className="flex flex-col gap-4">
         <div className="flex flex-col gap-4  justify-between">
           <TextInput type="text" placeholder="Title" required id="title" className="flex-1"/>
@@ -18,11 +50,17 @@ export default function CreatePost() {
             <option value="Express js">Express js</option>
           </Select>
           <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
-            <FileInput type="file"></FileInput>
-            <Button type="button" gradientDuoTone={"purpleToBlue"} size="sm" outline>Upload Image</Button>
+            <FileInput onChange={HandleImage} type="file"></FileInput>
+            <Button disabled={loading} onClick={HandleUpload} type="button" gradientDuoTone={"purpleToBlue"} size="sm" outline>Upload Image</Button>
+            {
+              formData.image && <img
+              src={formData.image}
+              className="w-full h-72 object-cover"
+              ></img>
+            }
           </div>
           <ReactQuill theme="snow" required placeholder="Write the blog here!" className="h-72 mb-12"></ReactQuill>
-          <Button type="submit" gradientDuoTone={"purpleToPink"}>Publish</Button>
+          <Button disabled={loading} type="submit" gradientDuoTone={"purpleToPink"}>Publish</Button>
         </div>
       </form>
     </div>
